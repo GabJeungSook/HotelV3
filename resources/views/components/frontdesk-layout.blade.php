287b1729ab8161be347469c4f519a468e398ef69
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2026-01-27 09:16:59 +0800

    frontdesk layout temp

--- resources/views/components/frontdesk-layout.blade.php
+++ resources/views/components/frontdesk-layout.blade.php
@@ -180,6 +180,7 @@
                 </svg>
                 <span>Room Monitoring</span>
               </a>
+
               {{-- <a href="{{ route('frontdesk.food-inventory') }}"
               class="text-gray-500 fill-gray-400 group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-800 "
               aria-expanded="false">
@@ -193,6 +194,15 @@
             </div>
           </nav>
           <div class="flex items-center md:ml-12">
+            <a href="{{ route('frontdesk.room-monitoring') }}"
+                class="text-gray-500 fill-gray-500 group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900"
+                aria-expanded="false">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="24" height="24" class="mr-2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" />
+                </svg>
+
+                <span>Cash on Hand</span>
+              </a>
             <form method="POST" action="{{ route('logout') }}">
               @csrf
               <x-button negative label="Logout" class="ml-2"
