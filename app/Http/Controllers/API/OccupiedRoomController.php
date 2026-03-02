a1946f092e442fcd6f68d6ee19d274e110a757fc
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2026-03-02 13:20:50 +0800

    ss

--- app/Http/Controllers/API/OccupiedRoomController.php
+++ app/Http/Controllers/API/OccupiedRoomController.php
@@ -18,9 +18,7 @@ class OccupiedRoomController extends Controller
         try {
             //query
             $floors = Floor::where('branch_id', $branchId)->with(['rooms' => function ($query) use ($branchId) {
-                    $query->where('status', 'Occupied')->with(['latestCheckInDetail.guest.type', 'latestCheckInDetail.transactions' => function ($q) {
-                        $q->whereNotIn('transaction_type_id', [2,5]);
-                    }, 'latestCheckInDetail.extendedGuestReports']);
+                    $query->where('status', 'Occupied')->with(['latestCheckInDetail.guest.type', 'latestCheckInDetail.transactions', 'latestCheckInDetail.extendedGuestReports']);
                 }])
                 ->orderBy('number')
                 ->get();
