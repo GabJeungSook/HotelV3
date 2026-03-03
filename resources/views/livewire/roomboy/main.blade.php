23c179480e945d84931565071f0e33eb217366a8
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2026-03-03 20:53:55 +0800

    room_id

--- resources/views/livewire/roomboy/main.blade.php
+++ resources/views/livewire/roomboy/main.blade.php
@@ -173,7 +173,7 @@
                                                     </button>
                                                      <button
                                                         class="mt-3 bg-red-600 text-white hover:bg-red-500 flex items-center gap-2 px-4 py-2 rounded"
-                                                        wire:click="openAuthorizationModal({{ $room->id }}">
+                                                        wire:click="openAuthorizationModal({{ $room_id->id }}">
                                                         Override
                                                     </button>
                                                 </div>
