85df5ee695b0132e9b3b953e5dc8d8ba4a0f16fe
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2026-03-03 20:35:04 +0800

    try override

--- resources/views/livewire/roomboy/main.blade.php
+++ resources/views/livewire/roomboy/main.blade.php
@@ -171,6 +171,14 @@
                                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
                                                         </svg>
                                                     </button>
+                                                     <button
+                                                        class="bg-red-600 text-white hover:bg-red-500 flex items-center gap-2 px-4 py-2 rounded">
+                                                        Override
+                                                        <!-- Right arrow icon (Heroicons: arrow-narrow-right) -->
+                                                        <svg class="w-5 h-5 ml-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                                                        </svg>
+                                                    </button>
                                                 </div>
                                             </div>
 
